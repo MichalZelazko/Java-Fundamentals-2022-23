5ca2f5d00e45cb91f922416b08b4149774e87ce3
MichalZelazko <u18emiczel@liceum.p.lodz.pl>
2022-11-14 12:17:46 +0100

    update main

--- TelephoneEntries/TelephoneEntriesApp.java
+++ TelephoneEntries/TelephoneEntriesApp.java
@@ -1,10 +1,9 @@
 public class TelephoneEntriesApp {
     public static void main(String[] args) {
-        TelephoneNumber telephoneNumber = new TelephoneNumber( "48", "123 456 789" );
-        Address address = new Address(telephoneNumber, "123 Main Street");
-        Person person = new Person("John", "Smith", address);
-        Company company = new Company("ABC", address);
-        TelephoneEntry[] telephoneEntries = {person, company};
+        Person person1 = new Person("John", "Smith", new Address(new TelephoneNumber( "48", "123 456 789" ), "123 Main Street"));
+        Person person2 = new Person("Tyler", "Johnson", new Address(new TelephoneNumber( "48", "154 887 895" ), "12 South Street"));
+        Person person3 = new Person("Kyle", "Brooks", new Address(new TelephoneNumber( "48", "124 856 477" ), "67 West Street"));
+        TelephoneEntry[] telephoneEntries = {person1, person2, person3};
         for (TelephoneEntry telephoneEntry : telephoneEntries) {
             telephoneEntry.description();
         }
