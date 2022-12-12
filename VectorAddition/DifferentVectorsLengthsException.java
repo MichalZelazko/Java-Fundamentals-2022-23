858605eda9bc19b1040be76e3a3b7e4a54d8db88
Michał Żelazko <242149@edu.p.lodz.pl>
2022-12-12 17:02:16 +0100

    changed exception structure

--- VectorAddition/DifferentVectorsLengthsException.java
+++ VectorAddition/DifferentVectorsLengthsException.java
@@ -1,39 +1,17 @@
 public class DifferentVectorsLengthsException extends Exception {
 
-    private String[] dataForExceptionMessage;
+    private Integer[] lengthsAndIndexes;
 
     public DifferentVectorsLengthsException(String message, MyVector v1, MyVector v2, int index){
         super(message);
-        this.dataForExceptionMessage = prepareDataForExceptionMessage(v1, v2, index);
+        this.lengthsAndIndexes = new Integer[4];
+        this.lengthsAndIndexes[0] = v1.getLength();
+        this.lengthsAndIndexes[1] = v2.getLength();
+        this.lengthsAndIndexes[2] = index + 1;
+        this.lengthsAndIndexes[3] = index + 2;
     }
 
-    public String[] getDataForExceptionMessage(){
-        return this.dataForExceptionMessage;
-    }
-
-    private static String[] prepareDataForExceptionMessage(MyVector v1, MyVector v2, int index){
-        String[] data = new String[4];
-        data[0] = Integer.toString(v1.getLength());
-        data[1] = Integer.toString(index + 1);
-        data[2] = Integer.toString(index + 2);
-        if (v1.getLength() > v2.getLength()){
-            data[3] = "bigger";
-        } else {
-            data[3] = "smaller";
-        }
-        if(index == 0){
-            data[1] += "st";
-            data[2] += "nd";
-        } else if (index == 1){
-            data[1] += "nd";
-            data[2] += "rd";
-        } else if (index == 2){
-            data[1] = "rd";
-            data[2] += "th";
-        } else {
-            data[1] += "th";
-            data[2] += "th";
-        }
-        return data;
+    public Integer[] getDataForExceptionMessage(){
+        return this.lengthsAndIndexes;
     }
 }
--- VectorAddition/VectorApp.java
+++ VectorAddition/VectorApp.java
@@ -15,9 +15,10 @@ public class VectorApp {
                 writeToFile("sum.txt", sum.getVector().toString());
                 correctVectors = true;
             } catch (DifferentVectorsLengthsException e) {
+                String[] data = prepareDataForExceptionMessage(e.getDataForExceptionMessage());
                 System.out.println(e.getMessage());
-                System.out.println("The " + e.getDataForExceptionMessage()[1] + " vector length is " + e.getDataForExceptionMessage()[0]);
-                System.out.println("The " + e.getDataForExceptionMessage()[1] + " vector length is " + e.getDataForExceptionMessage()[3] + " than the " + e.getDataForExceptionMessage()[2] + " vector length");
+                System.out.println("The " + data[1] + " vector length is " + data[0]);
+                System.out.println("The " + data[1] + " vector length is " + data[3] + " than the " + data[2] + " vector length");
                 vectors = enterVectors(numberOfVectors, input);
             } catch (IOException e) {
                 System.out.println("File error occurred.");
@@ -49,22 +50,53 @@ public class VectorApp {
             System.out.print("Enter the values for vector " + (i + 1) + " (separated by comma, press Enter to finish): ");
             String vectorValues = input.next();
             String[] vectorValuesArray = vectorValues.split(",");
-            ArrayList<Integer> vectorValuesIntArray = new ArrayList<Integer>();
-            for (int j = 0; j < vectorValuesArray.length; j++) {
-                try {
-                    vectorValuesIntArray.add(Integer.parseInt(vectorValuesArray[j]));
-                } catch (NumberFormatException e) {
-                    continue;
-                }
-            }
+            ArrayList<Integer> vectorValuesIntArray = eliminateNotIntegers(vectorValuesArray);
             vectors.add(new MyVector(vectorValuesIntArray));
         }
         return vectors;
+    }
+
+    private static ArrayList<Integer> eliminateNotIntegers (String[] vectorValuesArray){
+        ArrayList<Integer> vectorValuesIntArray = new ArrayList<Integer>();
+        for (int i = 0; i < vectorValuesArray.length; i++) {
+            try {
+                vectorValuesIntArray.add(Integer.parseInt(vectorValuesArray[i]));
+            } catch (NumberFormatException e) {
+                continue;
+            }
+        }
+        return vectorValuesIntArray;
     }
 
     private static void writeToFile(String fileName, String vector) throws IOException{
         FileWriter fileWriter = new FileWriter(fileName);
         fileWriter.write(vector);
         fileWriter.close();
+    }
+
+    private static String[] prepareDataForExceptionMessage(Integer[] lengthsAndIndexes){
+        String[] data = new String[4];
+        data[0] = Integer.toString(lengthsAndIndexes[0]);
+        data[1] = Integer.toString(lengthsAndIndexes[2]);
+        data[2] = Integer.toString(lengthsAndIndexes[3]);
+        if (lengthsAndIndexes[0] > lengthsAndIndexes[1]){
+            data[3] = "bigger";
+        } else {
+            data[3] = "smaller";
+        }
+        if(lengthsAndIndexes[2] == 1){
+            data[1] += "st";
+            data[2] += "nd";
+        } else if (lengthsAndIndexes[2] == 2){
+            data[1] += "nd";
+            data[2] += "rd";
+        } else if (lengthsAndIndexes[2] == 3){
+            data[1] = "rd";
+            data[2] += "th";
+        } else {
+            data[1] += "th";
+            data[2] += "th";
+        }
+        return data;
     }
 }
